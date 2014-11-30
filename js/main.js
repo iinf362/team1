6055d49a8137a48926a5e67db0ce265052867b28
Leandro Farias <leoffarias@hotmail.com>
2014-11-30 12:15:17 -0500

    accordion menus

--- js/main.js
+++ js/main.js
@@ -2,5 +2,6 @@
 $(document).ready(function(){
 	$(".menu").click(function(){
 		$(this).parent().next().slideToggle("slow");
+		$(".cont-custom").not($(this).parent().next()).slideUp("slow");
 	});
 });
