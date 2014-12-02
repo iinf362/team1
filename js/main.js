c7d1368af5bd50a1588c6d0aa9f312658c834e1b
Leandro Farias <leoffarias@hotmail.com>
2014-12-02 12:26:29 -0500

    search bar

--- js/main.js
+++ js/main.js
@@ -1,6 +1,15 @@
+//JQuery
 //Menu slide functions
 $(document).ready(function(){
 	$(".menu").click(function(){
 		$(this).parent().next().slideToggle("slow");
 	});
 });
+
+//JavaScript
+function searchGoogle() {
+    var link = "http://www.google.com/#q="+document.forms["search"]["text-search"].value+"+site:http://iinf362.github.io/team1";
+    var googleLink = link.replace(" ", "+");
+    document.search.action = googleLink;
+    return true;
+}
