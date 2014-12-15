2fa7ead323e0b3838e1b29fafbe0cf75a84547e0
Chris Marchione <cmarchione@albany.edu>
2014-12-15 00:05:05 -0500

    edit

--- js/main.js
+++ js/main.js
@@ -5,15 +5,7 @@ $(document).ready(function(){
 		$(this).parent().next().slideToggle("slow");
 	});
 });
-$("#slideshow > div:gt(0)").hide();
-setInterval(function() { 
-  $('#slideshow > div:first')
-    .fadeOut(1000)
-    .next()
-    .fadeIn(1000)
-    .end()
-    .appendTo('#slideshow');
-},  3000);
+
 //JavaScript
 function searchGoogle() {
     var link = "http://www.google.com/#q="+document.forms["search"]["text-search"].value+"+site:http://iinf362.github.io/team1";
